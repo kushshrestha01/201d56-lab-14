931639f0c9bc905b49e32f646ff1e8200696436c
Charles Clemens <cclemensjr@gmail.com>
2019-04-11 16:42:55 -0700

    Attempted showcart

--- starter-code/js/cart.js
+++ starter-code/js/cart.js
@@ -7,7 +7,7 @@ table.addEventListener('click', removeItemFromCart);
 var cart;
 
 function loadCart() {
-  var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+  var cartItems = JSON.parse(localStorage.getItem('cartStorage')) || [];
   cart = new Cart(cartItems);
 }
 
@@ -19,16 +19,37 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-
+  console.log("THis worked");
   // TODO: Find the table body
-
+  var cartTable = document.getElementById("cart");
   // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
+  console.log(cart.items[0]);
+  for (var i = 0; i < cart.items.length; i++) {
+    // TODO: Create a TR
+    var tr = document.createElement("tr");
+    
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    var tdDeleteLink = document.createElement("td");
+    var tdQuantity = document.createElement("td");
+    var tdItem = document.createElement("td");
+    var a = document.createElement("a");
+
+    var aText = document.createTextNode("X");
+    var tdQtyText = document.createTextNode(`${ cart.items[i].quantity }`);
+
+    a. setAttribute("href", "#");
+    a.appendChild(aText);
+    
+
+
+
+  }
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
